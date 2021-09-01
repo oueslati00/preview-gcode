fca03d9178fc6740f521d59b27d12f30634d37c4
oueslati00 <boueslati76@gmail.com>
2021-08-31 19:55:29 -0700

    for tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,90 +1,78 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewChildren} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import { WebGLPreview } from 'gcode-preview';
-import {FileSystemDirectoryEntry, FileSystemFileEntry, NgxFileDropEntry} from 'ngx-file-drop';
+import {FileSystemFileEntry, NgxFileDropEntry} from 'ngx-file-drop';
 import * as THREE from 'three';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, AfterViewInit {
-   layersLoaded = 0;
+export class AppComponent implements AfterViewInit {
    chunkSize = 250;
-  title = 'untitled12';
-  file: any;
-  preview : any;
-  private context: HTMLCanvasElement;
-  @ViewChild('myCanvas',{static: false}) canvas: ElementRef;
+   file: any;
+   preview: any;
+   private context: HTMLCanvasElement;
+   @ViewChild('myCanvas', {static: false}) canvas: ElementRef;
 
-  public ngOnInit(): void {
-  }
-  constructor() {
-  }
-  ngAfterViewInit(){
+  ngAfterViewInit(): any{
     this.context = (this.canvas.nativeElement as HTMLCanvasElement);
-    console.log('ng after view int work fine ');
-    const preview = new WebGLPreview({
+
+    this.preview = new WebGLPreview({
       canvas : this.context,
       targetId : 'gcode-preview',
       topLayerColor: new THREE.Color('lime').getHex(),
       lastSegmentColor: new THREE.Color('red').getHex(),
-      buildVolume: {x: 250, y: 220, z: 150},
+      buildVolume: {x: 250 , y: 210 , z: 150 },
       initialCameraPosition: [0, 400, 450]
     });
 
-    preview.render();
-    this.preview = preview;
-    console.log(this.canvas);
-    window.addEventListener('resize', () => {
-      this.preview.resize();
-    });
+    this.preview.render();
   }
 
-  async fetchGcode(url){
+   dropped(files: NgxFileDropEntry[]): any{
+    for (const droppedFile of files) {
+      if (droppedFile.fileEntry.isFile && droppedFile.relativePath !== undefined) {
+          const fileEntry = droppedFile.fileEntry as FileSystemFileEntry; // cast droppedFile to FileEntry
+          fileEntry.file(async (file: File) => {
+              const url = window.URL.createObjectURL(file); // createUrl To The FileEntry
+              const linesList = await this.fetchGcode(url); // get List Of Line of the current File
+              this.loadPreviewChunked(this.preview , linesList  , 50); // Load Preview
+        });
+      }
+    }
+  }
+
+  async fetchGcode(url): Promise<any>{
     const response = await fetch(url);
 
     if (response.status !== 200) {
       throw new Error(`status code: ${response.status}`);
     }
-
     const file = await response.text();
     return file.split('\n');
   }
-  loadPreviewChunked(target, lines, delay) {
+
+
+  loadPreviewChunked(target, lines, delay): any {
     let c = 0;
     const id = '__animationTimer__' + Math.random().toString(36).substr(2, 9);
-    const loadProgressive = (preview) => {
+
+    const loadProgressive = () => {
       const start = c * this.chunkSize;
       const end = (c + 1) * this.chunkSize;
       const chunk = lines.slice(start, end);
       target.processGCode(chunk);
-      target.resize();
-        c++;
-        if (c * this.chunkSize < lines.length) {
-          window[id] = setTimeout(loadProgressive, delay);
-        }
+      c++;
+      if (c * this.chunkSize < lines.length) {
+        window[id] = setTimeout(loadProgressive, delay);
+      }
       else {
-        console.log("a")
+        console.log('this file was complete');
       }
     };
+
+
     window.clearTimeout(window[id]);
-    loadProgressive(target);
-  }
-   dropped(files: NgxFileDropEntry[]) {
-    for (const droppedFile of files) {
-      if (droppedFile.fileEntry.isFile) {
-        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
-        fileEntry.file(async (file: File) => {
-          console.log(file);
-          const url = window.URL.createObjectURL(file);
-          window.open(url);
-          const lines1 = await this.fetchGcode(url);
-          this.loadPreviewChunked(this.preview , lines1  , 50);
-        });
-      } else {
-        const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
-        console.log(droppedFile.relativePath, fileEntry);
-      }
-    }
+    loadProgressive();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import {NgxFileDropModule} from 'ngx-file-drop';
 import {FormsModule} from '@angular/forms';
